2ede883bdbe1b7ca95e02b21b92dff9de545ffe8
Paul Devey <focusedfidgeter@gmail.com>
2024-08-18 22:35:48 -0400

    added: work experience updated: wiki-ization

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,6 +3,7 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Paul's Portfolio</span>,
+  primaryHue: 200,
   project: {
     link: 'https://github.com/FocusedFidgeter/nextra-docs-portfolio',
   },
@@ -13,6 +14,22 @@ const config: DocsThemeConfig = {
   footer: {
     text: 'Paul Devey | © 2024',
   },
+  sidebar: {
+    defaultMenuCollapseLevel: 2,
+    autoCollapse: false,
+    toggleButton: true,
+    titleComponent: ({ title, type }) => {
+      if (type === 'separator') {
+        return (
+          <div style={{textAlign: 'center' }}>{title}</div>
+        )
+      }
+      return <>{title}</>
+    }
+  },
+  toc: {
+    backToTop: true
+  }
 }
 
 export default config
